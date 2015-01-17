5c9be64cb6df382dd527fdd181349aa6f625b697
DevRodri <rodry.fib@gmail.com>
2015-01-17 23:59:27 +0100

    Efectos Agua
    añadido los efectos de agua

---
+++ Assets/Scripts/Flotacion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flotacion : MonoBehaviour {
+
+
+	bool colision = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(colision){
+			this.rigidbody.AddExplosionForce(20.0f - this.rigidbody.velocity.y,this.transform.position,3.0f);
+		}
+	}
+
+	void Update () {
+		Debug.Log (this.rigidbody.velocity.y);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (!colision){
+			colision = true;
+		} else {
+			colision = false;
+		}
+	}
+}
---
+++ Assets/Scripts/TiraCubos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiraCubos : MonoBehaviour {
+
+	public GameObject cubo;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonUp(0))
+		{
+			RaycastHit ht;
+			Ray rallo = camera.ScreenPointToRay(Input.mousePosition);
+			Debug.DrawRay(rallo.origin, rallo.direction * 100, Color.yellow);
+			if (Physics.Raycast(rallo,out ht,100.0f)){
+
+				Vector3 posicion = new Vector3(ht.point.x, 60.0f, ht.point.z);
+				
+				Instantiate(cubo, posicion, Quaternion.identity);
+			}
+			
+		}
+	}
+}
