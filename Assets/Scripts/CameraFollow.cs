9cc2751e781c1d6d25ddd7dc648191c4e8f498cd
DevRodri <rodry.fib@gmail.com>
2015-01-25 15:03:46 +0100

    Escena camara y controlador jugador
    Añadida una escena de testeo para controlar el jugador y el movimiento
    de la cámara.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject target;
+
+	public float height;
+
+	private Vector3 offset;
+
+	public float damp;
+	// Use this for initialization
+	void Start () {
+
+		offset = target.transform.position - transform.position;
+		height += offset.y;
+	
+	}
+
+	void Update () {
+		offset.y = height;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float desiredAngle = target.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		//offset.y += height;
+
+		Vector3 targetPos = target.transform.position - (rotation * offset);
+
+		transform.position = Vector3.Slerp (transform.position, targetPos, damp * Time.deltaTime);
+
+		//transform.position = target.transform.position - (rotation * offset);
+
+		//Vector3 targetPos = target.transform.position + offset;
+
+		//transform.position = Vector3.Lerp (transform.position, targetPos, damp * Time.deltaTime);
+
+		transform.LookAt (target.transform);
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int speed;
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vertical = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+		float horizontal = Input.GetAxis ("Horizontal") * rotateSpeed * Time.deltaTime;
+
+		transform.Translate (0f, 0f, vertical);
+		transform.Rotate (0f,horizontal,0f);
+	}
+}
